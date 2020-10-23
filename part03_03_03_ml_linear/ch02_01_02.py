a9bb31b0ce81322babbd14bdd2b3e75147ed21a1
LDJWJ <frontier1020@naver.com>
2020-10-23 13:51:21 +0900

    20201023_update
    20201023_update

---
+++ part03_03_03_ml_linear/ch02_01_02.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+#%% knn 알고리즘 시각화 
+import mglearn
+import matplotlib.pyplot as plt
+import pandas as pd
+%matplotlib inline
+# %matplotlib auto
+
+from IPython import get_ipython
+
+#%%
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+plt.show()
+
+#%%
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+
+#%% 
+plt.plot(3,5)
+plt.show()
